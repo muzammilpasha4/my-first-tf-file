fa5cc3cc3b28b443bb6190b9c5d6f7ca2a50fa66
Muzammil Pasha <156335256+muzammilpasha4@users.noreply.github.com>
2024-03-30 14:45:28 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = var.region
-  access_key = "AKIAR2CJPSKHX7UT2OOY"
-  secret_key = "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"
+  access_key = "change-access-key-here"
+  secret_key = "change-secret-key-here"
 }
