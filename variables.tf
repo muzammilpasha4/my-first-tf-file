7dd8f4d5c4a13e98e594678fa19e75a487682328
Muzammil Pasha <156335256+muzammilpasha4@users.noreply.github.com>
2024-04-01 16:48:37 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,15 +1,18 @@
-variable "ami_id"{
-    type    =   string
-    default =   "ami-0c101f26f147fa7fd" 
-} 
-
-variable   "region"{
-    type    =   string
-    default =   "us-east-1"
+variable "region"{
+    default = "us-east-1"
+    type = string
 }
 
-variable    "instance_type"{
-    type    = string
+variable "cidr"{
+    default =   "10.0.0.0/16"
+}
+
+variable "instance_type"{
     default = "t2.micro"
+    type = string
 }
 
+variable "ami_id"{
+    default = "ami-0c101f26f147fa7fd"
+    type = string
+}
